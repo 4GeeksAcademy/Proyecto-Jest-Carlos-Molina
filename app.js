e640da37a96a5b63cdbd12ae937f333435f554c9
Carlos Molina <carlosm12.inves@gmail.com>
2024-07-26 14:22:39 +0000

    JEST conversor de monedas OK

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
-// Declaramos una función con el nombre exacto "formEuroToDollar"
 const fromEuroToDollar = function(valueInEuro) {
-    // Convertimos el valor a dólares
     let valueInDollar = valueInEuro * 1.07;
-    // Retornamos el valor en dólares
     return valueInDollar;
 }
-// Tenemos que incluir la función en el exports para que sea exportada a otros archivos como test.js
-module.exports = { fromEuroToDollar }
+
+const fromDollarToYen = function(valueInDollar) {
+    let valueInYen = (valueInDollar / 1.07) * 156.5;
+    return valueInYen;
+}
+
+const fromYenToPound = function(valueInYens) {
+    let valueInPounds = (valueInYens / 156.5) * 0.87;
+    return valueInPounds;
+}
+
+module.exports = { fromEuroToDollar , fromDollarToYen , fromYenToPound }
--- test.js
+++ test.js
@@ -1,13 +1,22 @@
+const { fromEuroToDollar , fromDollarToYen , fromYenToPound } = require('./app.js');
+
 test("One euro should be 1.07 dollars", function() {
-    // Importo la funcion desde app.js
-    const { fromEuroToDollar } = require('./app.js');
+    const dollars = fromEuroToDollar(100);
+    const expected = 100 * 1.07;
 
-    // Uso la función como debe ser usada
-    const dollars = fromEuroToDollar(3.5);
+    expect(fromEuroToDollar(100)).toBe(107);
+})
 
-    // Si 1 euro son 1.07 dólares, entonces 3.5 euros debe ser (3.5 * 1.07)
-    const expected = 3.5 * 1.07;
+test("Dollars should return in yens", function() {
+    const yens = fromDollarToYen(107);
+    const expected = (107 / 1.07) * 156.5;
 
-    // Hago mi comparación (la prueba)
-    expect(fromEuroToDollar(3.5)).toBe(3.745); // 1 euro son 1.07 dólares, entonces 3.5 euros deberían ser = (3.5 * 1.07)
+    expect(fromDollarToYen(107)).toBe(15650);
+})
+
+test("Yens should return in pounds", function() {
+    const pounds = fromYenToPound(15650);
+    const expected = (15650 / 156.5) * 0.87;
+
+    expect(fromYenToPound(15650)).toBe(87);
 })
